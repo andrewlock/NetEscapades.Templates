4156bcb2d313a9761ba1e5110bfa8b548b9f2c47
Andrew Lock <andrewlock.net@gmail.com>
2017-04-18 01:15:07 +0100

    Added MVC project with auth

---
+++ content/MaterialDesignLiteMvc/MaterialDesignLiteMvc/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaterialDesignLiteMvc.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
